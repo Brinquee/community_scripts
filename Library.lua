d7be7be72269023691e52802d44a10a294bab791
Brinquee <gape.henry99@gmail.com>
2025-10-19 16:11:44 -0300

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -1,8 +1,16 @@
---[[ UTILIDADES DE INTERFACE CUSTOM PARA OTCv8 MOBILE ]]--
+--[[
 
----------------------------------------------------------
--- Scroll Bar Function
----------------------------------------------------------
+  Scroll Bar Function
+
+  Id: Nome que irá ficar salvo no storage, usado para salvar os valrores.
+  Title: Titulo principal.
+  Min: Valor minimo.
+  Max: Valor máximo.
+  Default Value: Valor definido no começo.
+  Dest: Tab que irá ficar a scrollbar.
+  Tooltip: Tooltip que irá ficar no widget
+]]--
+
 local scrollBarLayout = [[
 Panel
   height: 28
@@ -26,53 +34,68 @@ Panel
     step: 1
 ]]
 
+
 storage.scrollBarValues = storage.scrollBarValues or {}
 
-function addScrollBar(id, title, min, max, defaultValue, dest, tooltip)
-  local widget = setupUI(scrollBarLayout, dest)
-  widget.text:setTooltip(tooltip)
-
-  local value = storage.scrollBarValues[id]
-  if type(value) ~= "number" then value = defaultValue or min end
-  value = math.min(math.max(value, min), max)
-
-  widget.scroll.onValueChange = function(scroll, val)
-    widget.text:setText(title .. ": " .. val)
-    storage.scrollBarValues[id] = val
-  end
-
-  widget.scroll:setMinimum(min)
-  widget.scroll:setMaximum(max)
-  widget.scroll:setValue(value)
-  widget.scroll.onValueChange(widget.scroll, value)
+addScrollBar = function(id, title, min, max, defaultValue, dest, tooltip)
+    local widget = setupUI(scrollBarLayout, dest)
+    widget.text:setTooltip(tooltip)
+    local value = math.min(math.max(storage.scrollBarValues[id] or defaultValue, min), max)
+    widget.scroll.onValueChange = function(scroll, value)
+        widget.text:setText(title .. ": " .. value)
+        storage.scrollBarValues[id] = value
+    end
+    widget.scroll:setMinimum(min)
+    widget.scroll:setMaximum(max)
+    widget.scroll:setValue(value)
+    widget.scroll.onValueChange(widget.scroll, value)
 end
 
----------------------------------------------------------
--- Switch Bar Function
----------------------------------------------------------
+--[[
+
+  Switch Bar Function
+
+  Id: Nome que irá ficar salvo no storage, usado para salvar os valrores.
+  Title: Titulo principal.
+  Default Value: Valor definido no começo(boolean).
+  Dest: Tab que irá ficar a scrollbar.
+  Tooltip: Tooltip que irá ficar no widget
+]]--
+
+
 local switchBarLayout = [[
-Switch
+BotSwitch
   height: 20
   margin-top: 7
 ]]
 
+
 storage.switchStatus = storage.switchStatus or {}
 
-function addSwitchBar(id, title, defaultValue, dest, tooltip)
-  local widget = setupUI(switchBarLayout, dest)
-  widget.onClick = function()
-    widget:setOn(not widget:isOn())
-    storage.switchStatus[id] = widget:isOn()
-  end
-  widget:setText(title)
-  widget:setTooltip(tooltip)
-  widget:setOn(storage.switchStatus[id] or defaultValue)
+addSwitchBar = function(id, title, defaultValue, dest, tooltip)
+    local widget = setupUI(switchBarLayout, dest)
+    widget.onClick = function()
+        widget:setOn(not widget:isOn())
+        storage.switchStatus[id] = widget:isOn()
+    end
+    widget:setText(title)
+    widget:setTooltip(tooltip)
+    widget:setOn(storage.switchStatus[id] or defaultValue)
 end
 
----------------------------------------------------------
--- Item Widget Function
----------------------------------------------------------
-local itemWidget = [[
+--[[
+
+  Item Widget Function
+
+  Id: Nome que irá ficar salvo no storage, usado para salvar os valrores.
+  Title: Titulo principal.
+  Default Item: Valor definido no começo(id).
+  Dest: Tab que irá ficar a scrollbar.
+  Tooltip: Tooltip que irá ficar no widget
+
+]]--
+
+itemWidget = [[
 Panel
   height: 34
   margin-top: 7
@@ -90,39 +113,45 @@ Panel
     anchors.right: parent.right
 ]]
 
-storage.itemValues = storage.itemValues or {}
-
-function addItem(id, title, defaultItem, dest, tooltip)
-  local widget = setupUI(itemWidget, dest)
-  widget.text:setText(title)
-  widget.text:setTooltip(tooltip)
-  widget.item:setTooltip(tooltip)
-  local itemId = storage.itemValues[id] or defaultItem or 100
-  widget.item:setItemId(itemId)
-  storage.itemValues[id] = itemId
-  widget.item.onItemChange = function(w)
-    storage.itemValues[id] = w:getItemId()
-  end
+storage.itemValues = storage.itemValues or {};
+addItem = function(id, title, defaultItem, dest, tooltip)
+    local widget = setupUI(itemWidget, dest)
+    widget.text:setText(title)
+    widget.text:setTooltip(tooltip)
+    widget.item:setTooltip(tooltip)
+    widget.item:setItemId(storage.itemValues[id] or defaultItem)
+    widget.item.onItemChange = function(widget)
+        storage.itemValues[id] = widget:getItemId()
+    end
+    storage.itemValues[id] = storage.itemValues[id] or defaultItem
 end
 
----------------------------------------------------------
--- CheckBox Widget Function
----------------------------------------------------------
-local checkBoxWidget = [[
+--[[
+
+  CheckBox Widget Function
+
+  Id: Nome que irá ficar salvo no storage, usado para salvar os valrores.
+  Title: Titulo principal.
+  Default Item: Valor definido no começo(boolean).
+  Dest: Tab que irá ficar a scrollbar.
+  Tooltip: Tooltip que irá ficar no widget
+
+]]--
+
+checkBoxWidget = [[
 CheckBox
-  width: 150
-  height: 20
-  margin-top: 4
+  width: 30
 ]]
 
 storage.checkBoxStatus = storage.checkBoxStatus or {}
-
-function addCheckBox(id, title, defaultBoolean, dest, tooltip)
-  local widget = setupUI(checkBoxWidget, dest)
-  widget:setText(title)
-  widget:setTooltip(tooltip)
-  widget.onCheckChange = function(w, checked)
-    storage.checkBoxStatus[id] = checked
-  end
-  widget:setChecked(storage.checkBoxStatus[id] or defaultBoolean)
+addCheckBox = function(id, title, defaultBoolean, dest, tooltip)
+    local widget = setupUI(checkBoxWidget, dest)
+    widget:setText(title)
+    widget:setTooltip(tooltip)
+    widget.onCheckChange = function(widget, checked)
+        widget:setChecked(checked)
+        storage.checkBoxStatus[id] = checked;
+    end
+    widget:setChecked(storage.checkBoxStatus[id] or defaultBoolean)
 end
+
