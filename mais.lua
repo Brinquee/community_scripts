d89498c7bf81c2f9889f9772e493ee9e80ec4b41
Brinquee <gape.henry99@gmail.com>
2025-10-19 14:33:12 -0300

    Fix URL for community scripts in mais.lua

--- mais.lua
+++ mais.lua
@@ -1,4 +1,4 @@
-local urlScript = 'https://raw.githubusercontent.com/brinquee/Community-Scripts/main/community_scripts.lua';
+local urlScript = 'https://raw.githubusercontent.com/brinquee/Community_Scripts/main/community_scripts.lua';
 modules.corelib.HTTP.get(urlScript, function(script) 
     assert(loadstring(script))() 
 end);
