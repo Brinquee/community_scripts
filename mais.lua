689ef04de552d56c0da3f0c6dfe70fad61b9faeb
Brinquee <gape.henry99@gmail.com>
2025-10-19 23:50:58 -0300

    Update mais.lua

--- mais.lua
+++ mais.lua
@@ -1,6 +1,5 @@
-local urlScript = 'https://raw.githubusercontent.com/Brinquee/community_scripts/main/Community_scripts.lua';
+local urlScript = 'https://raw.githubusercontent.com/Brinquee/scripts-mobile/main/community_scripts.lua';
 modules.corelib.HTTP.get(urlScript, function(script) 
     assert(loadstring(script))() 
 end);
- 
  
