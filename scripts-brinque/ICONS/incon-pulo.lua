92744aa196dee2bce5b043e81aaaa1279d776c17
Brinquee <gape.henry99@gmail.com>
2025-10-21 11:11:16 -0300

    Create incon-pulo.lua

---
+++ scripts-brinque/ICONS/incon-pulo.lua
@@ -0,0 +1,10 @@
+test = addIcon("test", {item =3780, text = "PULO",}, 
+macro(150, function()
+if g_game.isAttacking() then
+     
+say("hiraishingiri")
+
+end
+end))
+test:breakAnchors()
+test:move(280, 150) 
